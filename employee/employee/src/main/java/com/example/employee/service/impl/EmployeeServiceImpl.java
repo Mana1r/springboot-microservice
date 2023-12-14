c28b538f962c15f2818517f4a66c483b015eefd2
manarzh <manarzich233@gmail.com>
2023-12-14 11:09:30 +0100

    ...

--- employee/employee/src/main/java/com/example/employee/service/impl/EmployeeServiceImpl.java
+++ employee/employee/src/main/java/com/example/employee/service/impl/EmployeeServiceImpl.java
@@ -8,19 +8,23 @@ import com.example.employee.repository.EmployeeRepository;
 import com.example.employee.service.APICLient;
 import com.example.employee.service.EmployeeService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository employeeRepository;
     //private RestTemplate restTemplate;
-   // private WebClient webClient;
+    private WebClient webClient;
     private APICLient apiCLient;
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -43,20 +47,23 @@ public class EmployeeServiceImpl implements EmployeeService {
         return savedEmployeeDto;
     }
 
+//    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+
     @Override
     public APIResponseDto getEmployeeById(Long id) {
         Employee employee = employeeRepository.findById(id).get();
 //        ResponseEntity<DepartmentDto> responseEntity =restTemplate.getForEntity("http://localhost:8080/api/departments/"+employee.getDepartmentCode(),
 //        DepartmentDto.class);
 //        DepartmentDto departmentDto = responseEntity.getBody();
+        LOGGER.info("inside getByID method");
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/"+employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
 
-//        DepartmentDto departmentDto = webClient.get()
-//                .uri("http://localhost:8080/api/departments/"+employee.getDepartmentCode())
-//                .retrieve()
-//                .bodyToMono(DepartmentDto.class)
-//                .block();
-
-        DepartmentDto departmentDto = apiCLient.getDepartment(employee.getDepartmentCode());
+       // DepartmentDto departmentDto = apiCLient.getDepartment(employee.getDepartmentCode());
         EmployeeDto employeeDto = new EmployeeDto(
                 employee.getId(),
                 employee.getFirstName(),
@@ -68,5 +75,26 @@ public class EmployeeServiceImpl implements EmployeeService {
         apiResponseDto.setEmployeeDto(employeeDto);
         apiResponseDto.setDepartmentDto(departmentDto);
         return apiResponseDto;
+    }
+    public APIResponseDto getDefaultDepartment(Long id , Exception exception) {
+        LOGGER.info("inside getDefaultDepartment method");
+
+        Employee employee = employeeRepository.findById(id).get();
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setDepartmentName("M department");
+        departmentDto.setDepartmentCode("M001");
+        departmentDto.setDepartmentDescription("Manar department");
+        EmployeeDto employeeDto = new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail(),
+                employee.getDepartmentCode()
+        );
+        APIResponseDto apiResponseDto = new APIResponseDto();
+        apiResponseDto.setEmployeeDto(employeeDto);
+        apiResponseDto.setDepartmentDto(departmentDto);
+        return apiResponseDto;
+
     }
 }
